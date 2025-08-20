88921b7817fb02714e08d4f5c3220903707d58e8
Arthur <arthurp1190@gmail.com>
2025-08-20 20:12:09 -0300

    First commit

---
+++ src/main/java/com/example/restful_login_api/RestfulLoginApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restful_login_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulLoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulLoginApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restful_login_api/RestfulLoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restful_login_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulLoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
