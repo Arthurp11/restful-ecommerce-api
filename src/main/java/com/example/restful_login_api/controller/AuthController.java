1d3b1f3db9d1fb8addea7ed638eff979f83fa3ee
Arthur <arthurp1190@gmail.com>
2025-08-25 10:49:07 -0300

    feat: added role inside user for better control

--- src/main/java/com/example/restful_login_api/controller/AuthController.java
+++ src/main/java/com/example/restful_login_api/controller/AuthController.java
@@ -28,7 +28,7 @@ public class AuthController {
     public ResponseEntity login(@RequestBody LoginRequestDTO body) {
         User user = repository.findByEmail(body.email())
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
-        if (!passwordEncoder.matches(body.password(), user.getPassword())) {
+        if (passwordEncoder.matches(body.password(), user.getPassword())) {
             String token = this.tokenService.generateToken(user);
 
             return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
