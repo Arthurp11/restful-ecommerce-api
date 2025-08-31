3ccb3041f09aef0c37adbc1811204ecff57a210a
Arthur <arthurp1190@gmail.com>
2025-08-31 16:13:04 -0300

    feat: implement Category service with CRUD operations and DTOs for category management

---
+++ src/main/java/com/example/restful_login_api/dto/category/CreateCategoryDTO.java
@@ -0,0 +1,3 @@
+package com.example.restful_login_api.dto.category;
+
+public record CreateCategoryDTO(String name) {}
--- src/main/java/com/example/restful_login_api/service/category/CategoryService.java
+++ src/main/java/com/example/restful_login_api/service/category/CategoryService.java
@@ -1,4 +1,59 @@
 package com.example.restful_login_api.service.category;
 
+import com.example.restful_login_api.domain.category.Category;
+import com.example.restful_login_api.domain.image.Image;
+import com.example.restful_login_api.domain.product.Product;
+import com.example.restful_login_api.dto.category.CategoryResponseDTO;
+import com.example.restful_login_api.dto.category.CreateCategoryDTO;
+import com.example.restful_login_api.dto.category.UpdateCategoryDTO;
+import com.example.restful_login_api.dto.product.ProductResponseDTO;
+import com.example.restful_login_api.infra.exception.ResourceNotFoundException;
+import com.example.restful_login_api.repository.CategoryRepository;
+import org.hibernate.sql.Update;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No category found with id: " + id));
+    }
+
+    public List<CategoryResponseDTO> getAllCategories(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        List<Category> categories = this.categoryRepository.findAll(pageable).getContent();
+        return categories.stream()
+                .map(category -> new CategoryResponseDTO(
+                        category.getId(),
+                        category.getName()
+                ))
+                .toList();
+    }
+
+    public Category createCategory(CreateCategoryDTO category) {
+        Category newCategory = new Category();
+        newCategory.setName(category.name());
+        return categoryRepository.save(newCategory);
+    }
+
+    public Category updateCategory(Long id, UpdateCategoryDTO category) {
+        Category existingCategory = categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No category found with id: " + id));
+        existingCategory.setName(category.name());
+        return categoryRepository.save(existingCategory);
+    }
+
+    public void deleteCategory(Long id) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No category found with id: " + id));
+        categoryRepository.delete(category);
+    }
 }
