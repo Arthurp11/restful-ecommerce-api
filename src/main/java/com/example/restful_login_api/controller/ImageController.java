d704bfee9d622f8603800e9738b73e0ca8f2f93b
Arthur <arthurp1190@gmail.com>
2025-09-10 00:19:59 -0300

    feat: add Image and Category controllers for handling product and category requests

---
+++ src/main/java/com/example/restful_login_api/controller/ImageController.java
@@ -0,0 +1,7 @@
+package com.example.restful_login_api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageController {
+}
--- src/main/java/com/example/restful_login_api/controller/ProductController.java
+++ src/main/java/com/example/restful_login_api/controller/ProductController.java
@@ -1,12 +1,10 @@
 package com.example.restful_login_api.controller;
 
-import com.example.restful_login_api.domain.Image;
 import com.example.restful_login_api.domain.Product;
 import com.example.restful_login_api.dto.product.CreateProductDTO;
 import com.example.restful_login_api.dto.product.ProductResponseDTO;
 import com.example.restful_login_api.dto.product.UpdateProductDTO;
 import com.example.restful_login_api.service.ProductService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +14,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
-@RequiredArgsConstructor
 public class ProductController {
 
     @Autowired
